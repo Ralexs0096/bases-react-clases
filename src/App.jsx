5c2ea98a95f0934e59715e4199d2b1da5f1eae51
Alexs Ruiz <ralexs.acu@gmail.com>
2023-05-30 12:39:34 -0600

    first commit - basic component

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+const App = () => {
+  // Necesita las llaves
+  return (
+    <>
+      <h1>Mi primer</h1>
+      <h2>Componente</h2>
+    </>
+  );
+};
+
+export default App; // No necesita las llaves
+
+// que es un componente?
+/**
+ * Es una funcion que retorna JSX
+ */
+
+/** Que es JSX?
+ *
+ * Es HTML dentro de un archivo JS
+ */
+
+/**
+ * //! RULE:
+ * Un componente de React solamente puede retornar 1 elemento JSX
+ *
+ */
+
+/**
+ * Fragment:
+ *
+ * <Fragment></Fragment>
+ * <></>
+ */
